e80e5e0ba9750c6652dff5aa7c55c55cc5a60a53
Guillaume Piccarreta <guillaume.piccarreta@gmail.com>
2025-12-04 01:39:39 +0100

    update bio

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -27,8 +27,8 @@ class Me extends React.Component {
             <div>
               <p>Hi, my name is Guillaume Piccarreta.</p>
               <p>Welcome to my web portal interface.</p>
-              <p>I am a software developer, teacher, <a href="http://grain-noir.com/" target="_blank" >audio and visual</a> artist working with code.</p>
-              <p>The main focus of my experiments is to create artworks and custom tools crossing the physical and digital world boundaries for research purposes.</p>
+              <p>I am a software developer, <a href="http://grain-noir.com/" target="_blank" >audio and visual</a> artist working with code.</p>
+              <p>My main focus is to create artworks and custom tools for audiovisual performance, experimental music, interactive installations, and research purposes.</p>
               <p>I like to make experimental software, write and fix bugs while listenning to white noise with a lowpass filter cutoff frequency set to 440Hz.</p>
               <p>Feel free to explore my portal interface or contact me at <a href="mailto:gcode.wav@gmail.com">gcode.wav@gmail.com</a> for inquiries.</p>
               <p>Have a nice day :)</p>
